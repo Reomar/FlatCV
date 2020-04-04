8aa2a8fd086e0e36bf83017b8ccd407425c28c15
Omar Sakr <mero.saker@gmail.com>
2020-04-05 00:06:08 +0200

    Added certificate Img
    added a lightbox of a certificate when the the text is clicked

---
+++ lightboxScript.js
@@ -0,0 +1,22 @@
+const lightbox = document.createElement("div")
+lightbox.id = "lightbox";
+document.body.appendChild(lightbox)
+
+const frontend = document.getElementById("front-certificate");
+
+
+frontend.onclick = () =>{
+    lightbox.classList.add('active');
+     const img =document.createElement("img");
+     img.src = "../imgs/certificate.png";
+     while(lightbox.firstChild){
+         lightbox.removeChild(lightbox.firstChild)
+     }
+     lightbox.appendChild(img);
+
+}
+
+lightbox.onclick= e =>{
+    if(e.target !== e.currentTarget) return;
+    lightbox.classList.remove('active')
+}
